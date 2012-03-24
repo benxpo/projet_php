30f85f0c8b9e92c8f4a95c6eab21a46de83d6791
Benjamin <ben.xpo@gmail.com>
2012-03-24 16:18:13 +0100

    Ajout de commentaires

--- ComicReader/src/ComicReader/AdminBundle/Controller/AdminController.php
+++ ComicReader/src/ComicReader/AdminBundle/Controller/AdminController.php
@@ -15,18 +15,32 @@ class AdminController extends Controller
      */
     public function indexAction()
     {
+/*
+	//Creation de l'objet
+
 	$right = new T_Right();
 	$right->setName("toto");
 	echo $right->getName();
 
+	//Enregistrement 
+
 	$em = $this->getDoctrine()->getEntityManager();
 	$em->persist($right);
+
+	//refresh de la bdd 
+
 	$em->flush();
 
 	echo $right->getId();
 
 	echo "<br/>";
+*/
+	//recuperation de la liste des entrées de la table t_right (voir find (id) findby(...))
 
+	$t = $this->getDoctrine()->getRepository('ComicReaderAdminBundle:T_Right')->findAll();
+
+	foreach($t as $g)	
+		echo $g->getName()."<br/>";
 
 	echo "<br/>";
       	return array();
