2b9c4d00f7db7e5b6a83774109aa2a3cc6d7d930
Antoine Fournier <antoine.fournier@epita.fr>
2012-03-24 22:06:04 +0100

    Ajout d un fichier manquant

--- ComicReader/src/ComicReader/AdminBundle/Controller/DefaultController.php
+++ ComicReader/src/ComicReader/AdminBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace ComicReader\AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {   
@@ -73,5 +74,34 @@ class DefaultController extends Controller
             
         }
         return $this->render('ComicReaderAdminBundle:Default:empty.html.twig');
+    }
+    
+    /**
+     * @Route("/upload")
+     * @Template()
+     */
+    public function uploadZipAction(Request $request)
+    {
+        $defaultData = array('manga_name' => 'Manga\'s name here');
+        $form = $this->createFormBuilder($defaultData)
+        ->add('author_name', 'text')
+        ->add('manga_name', 'text')
+        ->add('zip_file', 'file')
+        ->getForm();
+        
+       // echo $request;
+        
+        if ($request->getMethod() == 'POST')
+	{
+            $form->bindRequest($request);
+
+            $data = $form->getData();
+            echo "Author name : ".$data['author_name']."<br />";
+            echo "Manga name : ".$data['manga_name']."<br />";
+        }
+        
+        return $this->render('ComicReaderAdminBundle:Default:upload.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
