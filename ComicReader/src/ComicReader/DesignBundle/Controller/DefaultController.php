6d338a3512fd23da288f96293482f9fd89565b90
root <root@kiel-laptop.(none)>
2012-03-25 22:59:34 +0200

    controller design

--- ComicReader/src/ComicReader/DesignBundle/Controller/DefaultController.php
+++ ComicReader/src/ComicReader/DesignBundle/Controller/DefaultController.php
@@ -10,6 +10,42 @@ class DefaultController extends Controller
     
     public function indexAction($name)
     {
-	return $this->render('ComicReaderDesignBundle:Index:'.$name.'.html.twig');
+        $books = $this->getDoctrine()
+                         ->getEntityManager()
+                         ->getRepository('ComicReaderAdminBundle:Book')
+                         ->findAllValidated();
+                $fullJoinBooks = array();
+        
+        $fullJoinBooks = array();
+        foreach ($books as $b)
+        {
+            // Get author
+            $author = $this->getDoctrine()
+                        ->getEntityManager()
+                        ->getRepository('ComicReaderAdminBundle:Author')
+                        ->find($b->getFK_Author());
+
+            // Get uploader
+            $uploader = $this->getDoctrine()
+                                ->getEntityManager()
+                                ->getRepository('ComicReaderAdminBundle:T_User')
+                                ->find($b->getFK_User());
+                                
+            // get mark and comment
+            $comments = $this->getDoctrine()
+                             ->getEntityManager()
+                             ->getRepository('ComicReaderAdminBundle:Mark')
+                             ->searchMarks($b->getId());
+            
+            $fjb = new FullJoinBook();
+            $fjb->setBook($b);
+            $fjb->setAuthor($author);
+            $fjb->setUploader($uploader);
+            $fjb->setComments($comments);
+            $fullJoinBooks[] = $fjb;
+        }
+ 
+	return $this->render('ComicReaderDesignBundle:Index:'.$name.'.html.twig',
+                             array('books' => $fullJoinBooks));
     }
 }
